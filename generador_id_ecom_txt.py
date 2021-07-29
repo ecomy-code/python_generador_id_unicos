9d090832bc72be6ccf1da716967699b803d6ae30
Yeisson Araya <80219453+ecomy-code@users.noreply.github.com>
2021-07-29 00:48:23 -0600

    Create generador_id_ecom_txt.py

---
+++ generador_id_ecom_txt.py
@@ -0,0 +1,32 @@
+import os
+from datetime import date
+from datetime import datetime
+
+
+class Generator():
+
+	def iniciar_generador(self):
+		if os.path.isfile ("database/id.txt") == False:
+			txt = open("database/id.txt", "w")
+			txt.write("9")
+			txt.close()
+			return '9'
+		else:
+			txt = open ("database/id.txt", "r")
+			ids = txt.read()
+			txt.close()
+			return str(ids)
+
+
+def new_id(self):
+		ids = self.iniciar_generador()
+		ids = int(ids)+1
+		txt = open ("database/id.txt", 'w')
+		num = "{}".format(ids)
+		txt.write(num)
+		txt.close()
+		return str(ids)
+  
+  
+  
+  
